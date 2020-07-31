9dd5387d2083d85f23b7493eb2bf6c98501afc09
Pasquale Sacco <pasqsac@yahoo.it>
2020-07-31 11:25:10 +0200

    fixed error '\DetectionService.java uses unchecked'

--- android/src/main/java/com/xebia/activityrecognition/ActivityRecognizer.java
+++ android/src/main/java/com/xebia/activityrecognition/ActivityRecognizer.java
@@ -7,8 +7,8 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
-import android.support.annotation.Nullable;
-import android.support.v4.content.LocalBroadcastManager;
+import androidx.annotation.Nullable;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import java.io.*;
 import java.util.ArrayList;
